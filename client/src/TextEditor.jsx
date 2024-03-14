cff501fa0c94b7a5e722823ca08314765c4a66cd
shahin <shahinahd3@gamil.com>
2024-03-14 19:32:22 +0530

    Setting Up Quill

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
 
 const TextEditor = () => {
+    const wrapperRef = useCallback((wrapper)=>{
+        if(wrapper == null) return
+        wrapper.innerHTML =''
+
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor,{theme: "snow"})
+
+        return () => {
+            wrapperRef.innerHTML = ""
+        }
+    },[])
   return (
-    <div>TextEditor</div>
+    <div id='container' ref={wrapperRef} >TextEditor</div>
   )
 }
 
