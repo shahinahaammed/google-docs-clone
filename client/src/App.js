f7a78c6c124ab596489a70dbd75fe5f16ea6d659
shahin <shahinahd3@gamil.com>
2024-03-15 10:21:41 +0530

    CSS Styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import TextEditor from "./TextEditor";
 
 function App() {
--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -2,6 +2,19 @@ import React, { useCallback, useEffect, useRef } from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ]
+
 const TextEditor = () => {
     const wrapperRef = useCallback((wrapper)=>{
         if(wrapper == null) return
@@ -9,14 +22,14 @@ const TextEditor = () => {
 
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor,{theme: "snow"})
+        new Quill(editor,{theme: "snow", modules:{toolbar: TOOLBAR_OPTIONS} })
 
         return () => {
             wrapperRef.innerHTML = ""
         }
     },[])
   return (
-    <div id='container' ref={wrapperRef} >TextEditor</div>
+    <div className='container' ref={wrapperRef} >TextEditor</div>
   )
 }
 
