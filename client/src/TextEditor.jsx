c1e5752e6bc53952624c739e9b0b1179e601b02c
shahin <shahinahd3@gamil.com>
2024-03-15 10:53:54 +0530

    Adding Server Setup

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,36 +1,48 @@
-import React, { useCallback, useEffect, useRef } from 'react'
-import Quill from 'quill'
-import "quill/dist/quill.snow.css"
-
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
 const TOOLBAR_OPTIONS = [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ font: [] }],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["bold", "italic", "underline"],
-    [{ color: [] }, { background: [] }],
-    [{ script: "sub" }, { script: "super" }],
-    [{ align: [] }],
-    ["image", "blockquote", "code-block"],
-    ["clean"],
-  ]
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
 
 const TextEditor = () => {
-    const wrapperRef = useCallback((wrapper)=>{
-        if(wrapper == null) return
-        wrapper.innerHTML =''
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
-        const editor = document.createElement('div')
-        wrapper.append(editor)
-        new Quill(editor,{theme: "snow", modules:{toolbar: TOOLBAR_OPTIONS} })
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
 
-        return () => {
-            wrapperRef.innerHTML = ""
-        }
-    },[])
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q)
+  }, []);
   return (
-    <div className='container' ref={wrapperRef} >TextEditor</div>
-  )
-}
+    <div className="container" ref={wrapperRef}>
+      TextEditor
+    </div>
+  );
+};
 
-export default TextEditor
+export default TextEditor;
